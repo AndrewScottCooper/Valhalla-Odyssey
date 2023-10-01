3c4096e32025f9097a6e2696906e1585438e4130
AndrewScottCooper <50687410+AndrewScottCooper@users.noreply.github.com>
2023-10-01 21:13:15 +0800

    Quick fix to let attacks work for all enemy types

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -10,13 +10,16 @@ public class Projectile : MonoBehaviour
 
     private void Start()
     {
+        //get reference to how much damage the player's arrows do just incase we add damage upgrades
         damage = player.GetComponent<PlayerData>().arrowDamage;        
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // GameObject effect = Instantiate(impactEffect, transform.position, Quaternion.identity); // play a visual hit effect
         // Destroy(effect, 5f);\
-        if (collision.collider.CompareTag("ThrallV1"))
+
+        //only do damage to enemies if we hit something tagged as an enemy
+        if (collision.collider.CompareTag("Enemy"))
         {
           collision.gameObject.GetComponent<EnemyData>().TakeDamage(damage);
         }
