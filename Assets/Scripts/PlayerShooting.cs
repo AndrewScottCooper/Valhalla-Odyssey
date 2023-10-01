6846ea4924d9091be50106eebf78ccefdc79271c
ZackOhLantern <92610630+ZackOhLantern@users.noreply.github.com>
2023-10-01 09:39:54 -0700

    Arrows are hold to fire.
    with programmable fire rate.

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -8,21 +8,29 @@ public class PlayerShooting : MonoBehaviour
     public Transform aimPoint;
     public GameObject ProjectilePrefab;
 
-    public float ProjectileForce = 10f;
+    public float arrowFireRate = 0.5f; //fire rate in seconds
+    public float arrowProjectileForce = 20f; //arrow travel speed
+
+    float arrowCooldown = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1"))
         {
-            Shoot();
+            if (arrowCooldown >= arrowFireRate)
+                Shoot(); 
         }
+        arrowCooldown += Time.deltaTime;
     }
 
     void Shoot()
     {
+        
         GameObject Projectile = Instantiate(ProjectilePrefab, aimPoint.position, aimPoint.rotation);
         Rigidbody2D ProjectileRigidBody = Projectile.GetComponent<Rigidbody2D>();
-        ProjectileRigidBody.AddForce(aimPoint.up * ProjectileForce, ForceMode2D.Impulse);
+        ProjectileRigidBody.AddForce(aimPoint.up * arrowProjectileForce, ForceMode2D.Impulse);
+        arrowCooldown = 0f;
+
     }
 }
