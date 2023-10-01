c54ac80ea9d1bd0041498de02fa9b432dbc90dbb
AndrewScottCooper <50687410+AndrewScottCooper@users.noreply.github.com>
2023-10-01 13:27:26 +0800

    Small update for matt's sprites

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+    public float followSpeed = 2f;
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPosition, followSpeed);
+    }
+}
