c7d39665316453d199972e9de41f8a5e76d42b5a
ZackOhLantern <92610630+ZackOhLantern@users.noreply.github.com>
2023-10-01 21:49:47 -0700

    ship direction animations implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,18 +9,40 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D playerRigidBody;
     public Camera playerCam;
 
+    public Animator animator;
+
     Vector2 movement; // movement direction
     // Vector2 mousePos; // mouse position
 
     public bool canMove = true; //  for docking or menus
 
+    
 
     void Update()
     {
+
         if (canMove)
         {
             movement.x = Input.GetAxisRaw("Horizontal");  // movement input
             movement.y = Input.GetAxisRaw("Vertical");
+
+            if (movement.y > 0)
+            {
+                animator.SetInteger("Direction", 0);
+            }
+            else if (movement.y < 0)
+            {
+                animator.SetInteger("Direction", 2);
+            }
+
+            if (movement.x > 0)
+            {
+                animator.SetInteger("Direction", 1);
+            }
+            else if (movement.x < 0)
+            {
+                animator.SetInteger("Direction", 3);
+            }
 
             // mousePos = playerCam.ScreenToWorldPoint(Input.mousePosition); // mouse position based off of player camera
         }
