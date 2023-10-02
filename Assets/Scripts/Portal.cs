8123c563fee2bac0e648b2edbae2a8b3d9b8ba59
ZackOhLantern <92610630+ZackOhLantern@users.noreply.github.com>
2023-10-02 11:00:38 -0700

    level 01 portal fixed

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -28,13 +28,15 @@ public class Portal : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
+
         if (collision.CompareTag("Player"))
         {
+
             gameManager.GetComponent<SaveSystem>().SaveAllData();
             Debug.Log("Data saved, " + PlayerPrefs.GetInt("SoulsCollected"));
             if (Input.GetButton("Submit"))
             {
-                
+
                 NextLevel();
             }
 
